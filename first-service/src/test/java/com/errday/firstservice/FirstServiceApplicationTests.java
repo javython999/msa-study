3033f2070fb2ca512a05ddb8bff03a90f56d069f
javython999 <infinity2544@gmail.com>
2025-09-15 20:53:55 +0900

    api-gateway

---
+++ first-service/src/test/java/com/errday/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errday.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ second-service/src/test/java/com/errday/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errday.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
